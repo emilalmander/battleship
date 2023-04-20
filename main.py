c5c37379c1cce3986241d0159656b91e9e8dd816
emilalmander <emilalmander@hotmail.se>
2023-04-20 12:48:52 +0000

    added the game and senarios if you guess right or wrong

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 hiddenBoard = [[" "] * 8 for x in range(8)]
 guessBoard = [[" "] * 8 for x in range(8)]
 
+from random import randint
 
 lettersToNumbers = {
 "A": 0,
@@ -24,19 +25,71 @@ def createShips(board):
 
 
 def getShipLocation():
-    pass
+    row = input("please enter a ship row 1-8: ")
+    while row not in "12345678":
+        print("please enter a valid row")
+        row = input("please enter a ship row 1-8: ")
+    column = input("please input a ship column A-H: ").upper()
+    while column not in "ABCDEFGH" : 
+        print("please enter a valid column")
+        column = input("please input a ship column A-H: ").upper()
+    return int(row) -1, lettersToNumbers[column]
 
 
-def countHitShips():
-    pass
+
+    
+
+
+
+
+
+def countHitShips(board):
+    count = 0
+    for row in board:
+        for column in row:
+            if column == "X":
+                count += 1
+    return count
+
+
+
 
 def printBoard(board):
-    print("    A B C D E F G H")
-    print("    ---------------")
+    print("  A B C D E F G H")
+    print("  ---------------")
     rowNumber = 1
     for row in board:
         print("%d|%s" % (rowNumber, "|".join(row)))
         rowNumber += 1
+
+
+createShips(hiddenBoard)
+print("welcome to battleship, this is a singleplayer game! there are 3 different modes. easy, medium and hard. they have different amout of ships 3,5 and 7. ")
+turns = 10
+while turns > 0:
+    
+    printBoard(guessBoard)
+    row, column = getShipLocation()
+    if guessBoard[row][column] == "-":
+        print("you have already guessed that.")
+    elif hiddenBoard[row][column] == "X":
+        print("congratz, you have sunk a battleship")
+        guessBoard[row][column] = "X"
+        turns -= 1
+    else:
+        print("you missed the battleships")
+        guessBoard[row][column] = "-"
+        turns -= 1
+    if countHitShips(guessBoard) == 5:
+        print("all ships have been sunk!")
+        break
+    print("you have " + str(turns) + "turns remaining")
+    if turns == 0:
+        print("you ran out of turns, you lost!")
+        break
+
+
+
 
 
 
