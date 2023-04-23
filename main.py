89732b2c3931eaef37b491f479638b655d50ba24
emilalmander <emilalmander@hotmail.se>
2023-04-23 18:20:24 +0000

    added home screen, added rules, added a way to chose username, added a way to chose gamemode.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 
 
-hiddenBoard = [[" "] * 8 for x in range(8)]
-guessBoard = [[" "] * 8 for x in range(8)]
+cur.execute("""create table if not exists""")
+
+
+def resetBoard():
+    global hiddenBoard, guessBoard
+    hiddenBoard = [[" "] * 8 for x in range(8)]
+    guessBoard = [[" "] * 8 for x in range(8)]
 
 from random import randint
 
@@ -16,12 +21,33 @@ lettersToNumbers = {
 "H": 7
 }
 
-def createShips(board):
+def createShipsEasy(board):
+    for ship in range(3):
+        shipRow, shipColumn = randint(0,7), randint(0,7)
+        while board[shipRow][shipColumn] == "X":
+            shipRow, shipColumn = randint(0,7), randint(0,7)
+        board[shipRow][shipColumn] = "X"
+def createShipsMedium(board):
     for ship in range(5):
         shipRow, shipColumn = randint(0,7), randint(0,7)
         while board[shipRow][shipColumn] == "X":
             shipRow, shipColumn = randint(0,7), randint(0,7)
         board[shipRow][shipColumn] = "X"
+def createShipsHard(board):
+    for ship in range(7):
+        shipRow, shipColumn = randint(0,7), randint(0,7)
+        while board[shipRow][shipColumn] == "X":
+            shipRow, shipColumn = randint(0,7), randint(0,7)
+        board[shipRow][shipColumn] = "X"
+
+def createShips(board, mode):
+    if mode == "easy":
+        createShipsEasy(board)
+    elif mode == "medium":
+        createShipsMedium(board)
+    elif mode == "hard":
+        createShipsHard(board)
+
 
 
 def getShipLocation():
@@ -37,7 +63,14 @@ def getShipLocation():
 
 
 
-    
+def chooseGameMode():
+    while True:
+        mode = input("hello, " + username + " Choose a game mode (easy, medium, hard): ").lower()
+        if mode in ["easy", "medium", "hard"]:
+            return mode
+        else:
+            print("Invalid game mode. Please try again.")
+            
 
 
 
@@ -63,30 +96,107 @@ def printBoard(board):
         rowNumber += 1
 
 
-createShips(hiddenBoard)
-print("welcome to battleship, this is a singleplayer game! there are 3 different modes. easy, medium and hard. they have different amout of ships 3,5 and 7. ")
-turns = 10
-while turns > 0:
+def printRules():
+    print("Rules:")
+    print("The objective of the game is to sink all of the hidden ships on the board.")
+    print("You have a limited number of turns to make guesses.")
+    print("Each turn, you will guess a row and column to attack.")
+    print("If your guess hits a ship, the ship will be marked with an X on the board.")
+    print("If your guess misses, the location will")
     
-    printBoard(guessBoard)
-    row, column = getShipLocation()
-    if guessBoard[row][column] == "-":
-        print("you have already guessed that.")
-    elif hiddenBoard[row][column] == "X":
-        print("congratz, you have sunk a battleship")
-        guessBoard[row][column] = "X"
-        turns -= 1
-    else:
-        print("you missed the battleships")
-        guessBoard[row][column] = "-"
-        turns -= 1
-    if countHitShips(guessBoard) == 5:
-        print("all ships have been sunk!")
-        break
-    print("you have " + str(turns) + "turns remaining")
-    if turns == 0:
-        print("you ran out of turns, you lost!")
-        break
+    okay = input("when fully understood, write okay: ")
+    homescreen()
+    
+
+
+wins = 0
+loses = 0
+
+def win():
+    global wins
+    wins += 1
+
+def lose():
+    global loses
+    loses += 1
+
+def scoreboard():
+    print("Your score is " + str(loses) + " losses and " + str(wins) + " wins.")
+    homescreen()
+
+def chooseUsername():
+    username = input("Please enter a username: ")
+    return username
+    homescreen()
+
+def homescreen():
+    print ("a, chose another username")
+    print ("b, chose another gamemode")
+    print(" c, rules ")
+    print(" d, new game")
+    print(" e, scoreboard")
+    print(" f, exit")
+    choice = input("chose an option: ")
+    if choice == "a" or choice == "A":
+        return chooseUsername()
+    elif choice == "b" or choice == "B":
+        return chooseGameMode()
+    elif choice == "c" or choice == "C":
+        return printRules()
+    elif choice == "d" or choice == "D":
+        return runGame()
+    elif choice == "e" or choice == "E":
+        return scoreboard()
+
+def runGame():
+    resetBoard()
+    createShips(hiddenBoard, gameMode)
+
+
+    turns = 2
+    while turns > 0:
+        printBoard(guessBoard)
+        row, column = getShipLocation()
+        if guessBoard[row][column] == "-":
+            print("you have already guessed that.")
+            print(" ")
+        elif hiddenBoard[row][column] == "X":
+            print("congratz, you have sunk a battleship")
+            print(" ")
+            guessBoard[row][column] = "X"
+            turns -= 1
+        else:
+            print("you missed the battleships")
+            guessBoard[row][column] = "-"
+            turns -= 1
+        if countHitShips(guessBoard) == 5:
+            print("all ships have been sunk!")
+            homescreen()
+        print("you have " + str(turns) + " turns remaining")
+        if turns == 0:
+            print("you ran out of turns, you lost!")
+            lose()
+            homescreen()
+            
+    
+
+
+
+
+
+
+
+username = chooseUsername()
+gameMode = chooseGameMode()
+printRules()
+homescreen()
+
+
+
+
+
+
+
 
 
 
